3c440ab4a3e7cdc507cf21c9507bfe8a79ee34a9
Nir <nr1597@nyu.edu>
2019-01-26 11:19:49 -0500

    Commit#2

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -4,10 +4,11 @@ package com.app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("With changes");
     }
 }
